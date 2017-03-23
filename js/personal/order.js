6b26b33b29d4b545e7280edb639e033231c45e1d
shengqidetaiyang <494407127@qq.com>
2017-03-23 11:16:04 +0800

    v001
    移入之前的文件夹

---
+++ js/personal/order.js
@@ -0,0 +1,32 @@
+/* 
+* @Author: Marte
+* @Date:   2017-03-16 18:33:57
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-03-22 18:04:11
+*/
+
+$(document).ready(function(){
+    $(".orderList .title img").on("click",function(){
+        $(this).parent().parent().slideUp();
+    })  
+    $(".chooseBox .middle").on("click",function(){
+        window.location.href = "../../html/personal/orderDetail.html";
+    })
+    var lens = $(".orderBox").length;
+    for(var i = 0;i < lens;i++){
+        var len = $(".orderBox").eq(i).find("li").length;
+        len = len - 1;
+        $(".orderBox").eq(i).find("li").eq(len).find(".first").addClass('borderNone');
+        $(".orderBox").eq(i).find("li").eq(len).find(".second").addClass('borderNone');
+        $(".orderBox").eq(i).find("li").eq(len).find(".third").addClass('borderNone');
+        $(".orderBox").eq(i).find("li").eq(len).find(".forth").addClass('borderNone');
+        $(".orderBox").eq(i).find("li").eq(len).find(".fifth").addClass('borderNone');
+        $(".orderBox").eq(i).find("li").eq(len).find(".sixth").addClass('borderNone');
+    }
+    $(".success .down").on("click",function(){
+        window.location.href = "../../html/personal/delivery.html";
+    })
+    $(".finish .down").on("click",function(){
+        window.location.href = "../../html/personal/comment.html";        
+    })
+});
